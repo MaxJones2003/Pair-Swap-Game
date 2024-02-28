52426e9ea73aa06d38f2e7c9a7139caf3d50c194
MaxJones2003 <111801205+MaxJones2003@users.noreply.github.com>
2024-02-28 14:47:06 -0500

    Fixed the platform velocity accuracy problem

--- PairSwapGame/Assets/Scripts/Platform/PlatformHandler.cs
+++ PairSwapGame/Assets/Scripts/Platform/PlatformHandler.cs
@@ -8,18 +8,37 @@ public class PlatformHandler : MonoBehaviour
     public Camera cam;
     [SerializeField] private float yPosition = -8.5f;
     [SerializeField] private Vector2 xRange = new Vector2(-8.5f, 8.5f);
+    [SerializeField] private float speed = 1;
     private Vector2 lastPosition;
     private Vector2 velocity;
     [Range(0, 1)]
     public float biasFactor; // This factor can be adjusted to favor one vector over the other
 
+    private static readonly Vector2 vectorZero = new Vector2(0, 0);
+
+    private Vector2 preLerpVector;
+    private const float lerpDuration = 0.05f;
+    private float timeElapsed;
     void Update()
     {
         // Get the mouse position in world coordinates
         Vector2 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePosition = ClampHeight(mousePosition);
         // Calculate the velocity based on the change in position
-        velocity = (mousePosition - lastPosition) / Time.deltaTime;
+        // The preLerpVector is almost always zero, so we only set velocity to it if it isn't zero
+        // If the velocity is zero, we lerp the velocity to zero because the platform probably hasn't moved for a bit
+        preLerpVector = (mousePosition - lastPosition) / Time.deltaTime;
+        if (preLerpVector == vectorZero)
+        {
+            velocity = Vector2.Lerp(velocity, vectorZero, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+        }
+        else
+        {
+            timeElapsed = 0;
+            velocity = preLerpVector;
+        }
+        
         // Move the box to the mouse position
         transform.position = mousePosition;
 
